6494fa8f3ec13127610dc9ec6ca5f472e518b4bb
RavinduTharanga <ravindutharangaperera@gmail.com>
2026-05-17 20:21:38 -0400

    Initial datagolf frontend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,18 @@
+// import axios from "axios";
+
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_BASE_URL ||
+//   "https://u53gj338bl.execute-api.us-east-1.amazonaws.com/default/datagolf-backend";
+
+// export const api = axios.create({
+//   baseURL: API_BASE_URL,
+// });
+
 import axios from "axios";
 
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://u53gj338bl.execute-api.us-east-1.amazonaws.com/default/datagolf-backend";
+  "https://f5f8q9vaka.execute-api.us-east-1.amazonaws.com/default/datagolf-backend";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
