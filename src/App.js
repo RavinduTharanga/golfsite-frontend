29373a552b1c12e87745d89ddd7aea07d0697383
RavinduTharanga <ravindutharangaperera@gmail.com>
2026-05-17 22:04:23 -0400

    Initial datagolf frontend

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   useEffect(() => {
     async function fetchPlayers() {
       try {
-        const res = await api.get("/default/datagolf-backend");
+        const res = await api.get("/datagolf-backend");
         setPlayers(res.data.data || []);
       } catch (err) {
         console.error("Failed to fetch players", err);
